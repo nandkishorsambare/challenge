df9b1412212e5dfaf326b2054c4b12e45a087b14
nandkishorsambare <nsambare511@gmail.com>
2022-07-25 14:27:41 +0530

    my first git

---
+++ src/com/aspire/java/demo/Sample_1.java
@@ -0,0 +1,14 @@
+package com.aspire.java.demo;
+
+public class Sample_1
+{
+public static void main(String[] args)
+{
+	System.out.println("Hii Githubb");
+	int a=10;
+	
+	int c=10;
+	int multi=a*c;
+	System.out.println(multi);
+}
+}
